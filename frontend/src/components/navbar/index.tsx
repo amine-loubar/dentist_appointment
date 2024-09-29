2288fb5b02593ed5f31ab34fad3295a493439ba8
Amine Loubar <77306622+amine-loubar@users.noreply.github.com>
2024-09-29 19:05:28 +0100

    Update index.tsx

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -7,19 +7,7 @@ export const Navbar: React.FC = () => {
             <div className="flex-1">
                 <Link to="/" className="text-xl btn btn-ghost">Dentist Appointment</Link>
             </div>
-            <div className="flex-none">
-                <ul className="px-1 menu menu-horizontal">
-                    <li>
-                        <a href="#home">Home</a>
-                    </li>
-                    <li>
-                        <a href="#about">About</a>
-                    </li>
-                    <li>
-                        <a href="#services">Services</a>
-                    </li>
-                </ul>
-            </div>
+            
         </div>
     );
 };
